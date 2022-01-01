7aa983e8d8d0f70b27ab759dfb7bbdf02a10d78f
JamesBr0wn <j_brown_james@web.de>
2022-01-01 06:45:12 +0100

    Upgrades, screenshot improvement

--- app/src/main/java/com/diudkr/fractalland01/MainActivity01.java
+++ app/src/main/java/com/diudkr/fractalland01/MainActivity01.java
@@ -33,7 +33,6 @@ public class MainActivity01 extends AppCompatActivity {
     private GestureDetectorCompat mDetector;
     private PopupWindow mPopupWindow;
     private Context mContext;
-    private FrameLayout mMainContainer;
 
     class MyGestureListener extends GestureDetector.SimpleOnGestureListener {
 
@@ -186,7 +185,7 @@ public class MainActivity01 extends AppCompatActivity {
         Log.i("diudkr", "MainActivity01.makeScreenshot2 " + rootView);
         View screenView = rootView.getRootView();
         Log.i("diudkr", "MainActivity01.makeScreenshot3 " + screenView);
-        Boolean oldcachestate = screenView.isDrawingCacheEnabled();
+        boolean oldcachestate = screenView.isDrawingCacheEnabled();
         Log.i("diudkr", "MainActivity01.makeScreenshot3.5 isDrawingCacheEnabled() = " + oldcachestate);
         screenView.setDrawingCacheEnabled(true);
         Bitmap bitmap = Bitmap.createBitmap(screenView.getDrawingCache());
@@ -213,8 +212,7 @@ public class MainActivity01 extends AppCompatActivity {
         LayoutInflater inflater = (LayoutInflater) mContext.getSystemService(LAYOUT_INFLATER_SERVICE);
 
         // Inflate the popup view
-        // View popupView = inflater.inflate(R.layout.aboutpopup,null);
-        View popupView = inflater.inflate(R.layout.aboutpopup,null);
+        View popupView = inflater.inflate(R.layout.aboutpopup, null);
 
         // Initialize a new instance of popup window
         mPopupWindow = new PopupWindow(
@@ -230,7 +228,7 @@ public class MainActivity01 extends AppCompatActivity {
         }
 
         // Get a reference for the custom view close button
-        Button closeButton = (Button) popupView.findViewById(R.id.btn_close_about);
+        Button closeButton = popupView.findViewById(R.id.btn_close_about);
 
         // Set a click listener for the popup window close button
         closeButton.setOnClickListener(new View.OnClickListener() {
@@ -242,7 +240,49 @@ public class MainActivity01 extends AppCompatActivity {
         });
 
         // Finally, show the popup window at the center location of root relative layout
-        mMainContainer = (FrameLayout) findViewById(R.id.main_container);
+        FrameLayout mMainContainer = findViewById(R.id.main_container);
         mPopupWindow.showAtLocation(mMainContainer, Gravity.CENTER,0,0);
     }
+
+    private void doHide()
+    {
+        View decorView = getWindow().getDecorView();
+        int uiOptions = 0;
+        uiOptions |= View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+                | View.SYSTEM_UI_FLAG_FULLSCREEN
+                | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+        // | View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+        // | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+        // | View.SYSTEM_UI_FLAG_LOW_PROFILE
+        ;
+        if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.KITKAT) {
+            uiOptions = uiOptions | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY;
+        }
+        decorView.setSystemUiVisibility(uiOptions);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.i("diudkr", "MainActivity01.onPostCreate");
+        doHide();
+
+        View decorView = getWindow().getDecorView();
+        decorView.setOnSystemUiVisibilityChangeListener
+                (new View.OnSystemUiVisibilityChangeListener() {
+                    @Override
+                    public void onSystemUiVisibilityChange(int visibility) {
+                        // Note that system bars will only be "visible" if none of the
+                        // LOW_PROFILE, HIDE_NAVIGATION, or FULLSCREEN flags are set.
+                        if ((visibility & View.SYSTEM_UI_FLAG_FULLSCREEN) == 0) {
+                            // Note: The system bars are visible. Make any desired
+                            // adjustments to your UI, such as showing the action bar or
+                            // other navigational controls.
+                            doHide();
+                        }
+                    }
+                });
+
+    }
+
 }
